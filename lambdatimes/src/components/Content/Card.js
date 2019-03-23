0d2eb07e7735df78e31e1d7923201b0a82c5878e
Lambda_School_Loaner_69 <douglasjordan2@gmail.com>
2019-03-23 12:12:53 -0500

    finished the card filtering

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
+  const { headline, img, author } = props.card;
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{ headline }</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={ img } />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By { author }</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+
+Card.propTypes = {
+  card: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    headline: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    tab: PropTypes.string.isRequired
+  })
+}
 
 export default Card;
